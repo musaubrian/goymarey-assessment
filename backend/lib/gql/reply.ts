42a351c4389f3e41ab657540aadea90cd9d7188b
musaubrian <musaubrian15@gmail.com>
2025-04-09 22:18:12 +0300

    chore(backend): creates replies queries

---
+++ backend/lib/gql/reply.ts
@@ -0,0 +1,60 @@
+import {
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql'
+import { prisma } from '../db'
+
+const Reply = new GraphQLObjectType({
+  name: 'PostReply',
+  fields: {
+    id: { type: GraphQLID },
+    text: { type: GraphQLString },
+    createdAt: { type: GraphQLString },
+  },
+})
+
+const replyMutations = {
+  createReply: {
+    type: new GraphQLNonNull(Reply),
+    args: {
+      text: { type: new GraphQLNonNull(GraphQLString) },
+      postId: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (
+      _: any,
+      { text, postId }: { text: string; postId: string },
+      context: any
+    ) => {
+      // gotten from the context
+      const authorId = context?.user.id as string
+
+      await prisma.reply.create({
+        data: {
+          text,
+          post: {
+            connect: { id: postId },
+          },
+          author: {
+            connect: { id: authorId },
+          },
+        },
+      })
+    },
+  },
+  deleteReply: {
+    type: GraphQLBoolean,
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLID) },
+    },
+    resolve: async (_: any, { id }: { id: string }, context: any) => {
+      await prisma.reply.delete({
+        where: { id },
+      })
+    },
+  },
+}
+
+export { Reply, replyMutations }
