1699bb6f36e00bf7dc9ba223764ca93b08b08b99
musaubrian <musaubrian15@gmail.com>
2025-04-09 22:21:32 +0300

    chore(backend): initializes express with graphql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,45 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import { createHandler } from 'graphql-http/lib/use/express'
+import { schema } from '../lib/gql/schema'
+import { ruruHTML } from 'ruru/server'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 const PORT = process.env.PORT || 3030
 
-app.get('/', (_req, res) => {
-  res.json({ msg: 'well hello there' })
+app.use(express.json())
+app.get('/explorer', (_req, res) => {
+  res.type('html')
+  res.end(ruruHTML({ endpoint: '/graphql' }))
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running at :${PORT}`)
-})
+const authMiddleWare = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ')[1]
+  if (!token) {
+    // @ts-expect-error
+    req.user = null
+    return next()
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      id: string
+      username: string
+      createdAt: string
+    }
+
+    // @ts-expect-error
+    req.user = decoded
+    next()
+  } catch (e) {
+    res.status(401).json({ error: 'Invalid token' })
+  }
+}
+
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    // context: ({ req }) => ({ user: (req as any).user }),
+  })
+)
+app.listen(PORT, () => console.log(`Server running at :${PORT}`))
